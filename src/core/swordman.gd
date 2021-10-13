c4005b7218bf61621d764e4c79503edad02a7e57
bpodrygajlo <bartosz.podrygajlo@gmail.com>
2021-10-13 17:47:01 +0200

    Implement unit portrait display
    Unit portraits will now be displayed in the GUI.
    Limited the map size considerably to reduce loading times.
    Fixed how unit stats dictionary is created

--- src/core/swordman.gd
+++ src/core/swordman.gd
@@ -1,3 +1,5 @@
 extends Unit
 class_name SwordMan
 # Sworman class. First military unit implemented.
+
+var portrait : Texture = load("res://graphics/k\'inh humans assets/k\'inh unit portraits/family swordsman icon.png")
